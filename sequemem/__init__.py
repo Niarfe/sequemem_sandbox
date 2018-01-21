53a9bc6253be70a243b5d34d9693fa4094c66a38
EfrainOlivaresHG <efrain.olivares@hgdata.com>
2018-01-21 00:28:15 -0800

    safeguarding some progress, about to refactor for dynamic size

---
+++ sequemem/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.dont_write_bytecode = True
+
+from .sequemem import Neuron
+from .sequemem import Column
+from .sequemem import Bundle
+from .sequemem import final_prediction
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sequemem',
+    version='0.1',
+    description='Simple use of neuron columns to learn sequences',
+    url='git@github.com:Niarfe/sequemem.git',
+    author='Efrain Olivares',
+    author_email='efrain.olivares@gmail.com',
+    license='MIT',
+    packages=['sequemem'],
+    zip_safe=False
+    )
