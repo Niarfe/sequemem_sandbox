77e7126e61282447b8562899104a5aebefc1a7df
Niarfe <efrain.olivares@gmail.com>
2018-02-22 20:25:41 -0800

    YES finally got an output layer that works, have not plugged in tho

--- tests/test_sequemem.py
+++ tests/test_sequemem.py
@@ -2,14 +2,77 @@ import sys
 sys.path.append("./sequemem")
 
 from neuron import Neuron
+from neuron import SimpleNeuron
 from layer_multi import LayerMulti as Layer
 from layer_output import LayerOutput
+from layer_output import LayerSimpleOutput
 from sequemem import Sequemem
 def tokenize(sentence):
     return [word.strip('\t\n\r .') for word in sentence.split(' ')]
 
 def test_tokenize():
     assert tokenize("we are") == ["we", "are"]
+
+def test_simple_out_layer():
+    layer = Layer()
+
+    one = Neuron(layer)
+    one.add_key("one")
+    two = Neuron(layer)
+    two.add_key("two")
+    three = Neuron(layer)
+    three.add_key("three")
+    four = Neuron(layer)
+    four.add_key("four")
+    five = Neuron(layer)
+    five.add_key("five")
+
+    lfibo = [one, two, three, five]
+    lsequ = [one, two, three, four]
+
+    simpout = LayerSimpleOutput()
+
+    simpout.add_new("sequ")
+    simpout.add_new("fibo")
+
+    nsequ = simpout.get_neuron("sequ")
+    nfibo = simpout.get_neuron("fibo")
+
+    ## This is the part you embed into sequemem::hit LEARN
+    for n in lsequ:
+        n.add_upstream(nsequ)
+        nsequ.add_upstream(n)
+    for n in lfibo:
+        n.add_upstream(nfibo)
+        nfibo.add_upstream(n)
+
+    ## After the reset the stuff should just happen naturally in sequemem::hit predict
+    simpout.reset()
+    for n in lsequ:
+        n.set_active()
+        n.set_inactive()
+
+    ## Then after runnning it, you can get the counts of the highest, in this case 4
+    # and query for the predicted set
+    # which you can then overlap to get the right prediction.
+    # In this case, three would predict both four and five...
+    # but the sets from below differ depending on if it's fobo or sequ
+    print(simpout.global_state)
+    for n in simpout.global_state["predict"]:
+        print(n.keys, n.predict_times)
+    predicts = simpout.get_set_of_predicted(4)
+    print("Predicts:", predicts)
+    assert sorted(list(predicts)) == ["four", "one", "three", "two"]
+
+    simpout.clear("predict")
+    for n in lfibo:
+        n.set_active()
+        n.set_inactive()
+    for n in simpout.global_state["predict"]:
+        print(n.keys, n.predict_times)
+    predicts = simpout.get_set_of_predicted(4)
+    print("Predicts:", predicts)
+    assert sorted(list(predicts)) == ["five", "one", "three", "two"]
 
 def test_simple_sequemem():
     brain = Sequemem("sequemem")
