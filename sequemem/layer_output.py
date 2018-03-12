6522664610c3e769df2ea59485973854a1235861
Niarfe <efrain.olivares@gmail.com>
2018-03-12 01:40:42 -0700

    forgot one

--- sequemem/layer_output.py
+++ sequemem/layer_output.py
@@ -112,29 +112,22 @@ class LayerCount:
     def __init__(self, name="anon"):
         self.columns = defaultdict(set)
         self.global_counter = Counter()
-        self.output = None
         self.word_counter = Counter()
         self.window_size = 5
         self.name = name
         self.total_neurons = 0
         self.last_touched_neuron = None
-        self.d_w_uber_freq = {}
 
     def reset(self):
         self.clear_global_counter()
         self.clear_word_counter()
-
-    def get_global_counter(self):
-        return self.global_counter
-    def get_word_counter(self):
-        return self.word_counter
-    def set_window(self, window):
-        self.window_size = window
     def clear_word_counter(self):
         self.word_counter = Counter()
     def clear_global_counter(self):
         self.global_counter = Counter()
 
+    def set_window(self, window):
+        self.window_size = window
     def sequencer(self, str_rep):
         assert type(str_rep) == type(""), "sequencer type must be string"
         return [[word.strip()] for word in str_rep.split(' ')]
@@ -142,64 +135,25 @@ class LayerCount:
     def tokenize(self, sentence):
         return [str(word.strip().strip('\r\n\t.')) for word in sentence.strip().split(' ')]
 
-    def train_from_file(self, filepath):
+    def load_from_file(self, filepath):
         print("Loading file {}".format(filepath))
         with open(filepath,'r') as source:
             for idx, sentence in enumerate(source):
                 if idx % 10000 == 0: print(idx)
                 self.last_touched_neuron = None
-                self.simple_hit([word for word in self.tokenize(sentence)])
+                self.hit([word for word in self.tokenize(sentence)])
 
-    def simple_hit(self, sequence):
-        debug(sequence)
-        for _key in sequence:
-            debug("\t\tadding neuron to column {}".format(_key))
-            nw_nrn = CountingNeuron(_key)
-            self.columns[_key].add(nw_nrn)
+    def add(self, arr_sentence):
+        debug(arr_sentence)
+        for word in arr_sentence:
+            debug("\t\tadding neuron to column {}".format(word))
+            nw_nrn = CountingNeuron(word)
+            self.columns[word].add(nw_nrn)
 
             if self.last_touched_neuron != None:
                 self.last_touched_neuron.add_upstream(nw_nrn)
                 nw_nrn.add_downstream(self.last_touched_neuron)
             self.last_touched_neuron = nw_nrn
-
-    def get_frequency_dict(self, force_init=False):
-        if len(self.d_w_uber_freq) == 0 or force_init == True:
-            print("Re-iitializing dictionary")
-            self.d_w_uber_freq = {}
-            self.total_neurons = len(self.get_number_neurons_per_key())
-
-            for word, count in self.get_number_neurons_per_key().most_common():
-                self.d_w_uber_freq[word] = float(count)/self.total_neurons
-
-        return self.total_neurons, self.d_w_uber_freq
-
-    def comparison_frequencies(self, the_WORD, ratio=0.05, cutoff=15, visualize_it=False):
-        word_test, total_spec_w = self.get_counts_for_specific_key(the_WORD).most_common(1)[0] # should be itself
-        print("Count for ", word_test," is ", total_spec_w)
-        arr_the_word = []
-        arr_global_f = []
-        arr_spec_f   = []
-
-        for word, count in self.get_counts_for_specific_key(the_WORD).most_common():
-            this_freq = float(count/(total_spec_w + 0.01))
-            if float(self.d_w_uber_freq[word]/this_freq) <= ratio:
-                arr_the_word.append(word)
-                arr_global_f.append(self.d_w_uber_freq[word])
-                arr_spec_f.append(this_freq)
-                if len(arr_the_word) > cutoff:
-                    break
-
-        print("Going to start visual with {}".format(arr_the_word))
-        if visualize_it:
-            fig, ax = plt.subplots()
-            ax.scatter(arr_spec_f[1:cutoff], arr_global_f[1:cutoff])
-
-            for i, txt in enumerate(arr_the_word[1:cutoff]):
-                ax.annotate(txt, (arr_spec_f[i+1],arr_global_f[i+1]))
-            arr_the_word[1:cutoff]
-            plt.show()
-
-        return arr_global_f[:cutoff], arr_spec_f[:cutoff], arr_the_word[:cutoff]
 
     def get_counter_for_sequence(self, sequence, window_size=2, direction=0):
         """Get counts for n-grams (treating sequence as n-gram)
@@ -244,12 +198,57 @@ class LayerCount:
         self.word_counter[key] = self.word_counter[key]/2
         return self.word_counter
 
+
     def get_number_neurons_per_key(self):
         d_nums={}
         for key, neurons in self.columns.items():
             d_nums[key] = len(neurons)
         return Counter(d_nums)
 
+class Prism:
+    def __init__(self, layerCounter):
+        self.layer_counter = layerCounter
+        self.d_w_uber_freq = {}
+
+    def get_frequency_dict(self, force_init=False):
+        """Get a count of all words but in frequency terms, for global counts"""
+        if len(self.d_w_uber_freq) == 0 or force_init == True:
+            print("Re-iitializing dictionary")
+            self.d_w_uber_freq = {}
+            self.total_neurons = len(self.layer_counter.get_number_neurons_per_key())
+
+            for word, count in self.layer_counter.get_number_neurons_per_key().most_common():
+                self.d_w_uber_freq[word] = float(count)/self.total_neurons
+
+        return self.total_neurons, self.d_w_uber_freq
+
+    def comparison_frequencies(self, the_WORD, ratio=0.05, cutoff=15, visualize_it=False):
+        word_test, total_spec_w = self.layer_counter.get_counts_for_specific_key(the_WORD).most_common(1)[0] # should be itself
+        print("Count for ", word_test," is ", total_spec_w)
+        arr_the_word = []
+        arr_global_f = []
+        arr_spec_f   = []
+
+        for word, count in self.layer_counter.get_counts_for_specific_key(the_WORD).most_common():
+            this_freq = float(count/(total_spec_w + 0.01))
+            if float(self.d_w_uber_freq[word]/this_freq) <= ratio:
+                arr_the_word.append(word)
+                arr_global_f.append(self.d_w_uber_freq[word])
+                arr_spec_f.append(this_freq)
+                if len(arr_the_word) > cutoff:
+                    break
+
+        print("Going to start visual with {}".format(arr_the_word))
+        if visualize_it:
+            fig, ax = plt.subplots()
+            ax.scatter(arr_spec_f[1:cutoff], arr_global_f[1:cutoff])
+
+            for i, txt in enumerate(arr_the_word[1:cutoff]):
+                ax.annotate(txt, (arr_spec_f[i+1],arr_global_f[i+1]))
+            arr_the_word[1:cutoff]
+            plt.show()
+
+        return arr_global_f[:cutoff], arr_spec_f[:cutoff], arr_the_word[:cutoff]
 
 
     def get_predicted_counts_from_lighting_columns(self, lst_keys):
@@ -281,157 +280,7 @@ class LayerCount:
             return set_w1
 
 
-
     def get_top_x_words_in_layer(self, x):
         """Return list of the top x words with the highest count in the layer"""
         return [word for word, _ in layer.get_number_neurons_per_key().most_common()[:x]]
 
-# class LayerTree:
-#     def __init__(self, name="anon"):
-#         self.columns = defaultdict(set)
-#         self.global_counter = Counter()
-#         self.output = None
-#         self.word_counter = Counter()
-#         self.window_size = 5
-#         self.name = name
-#         self.total_neurons = 0
-#         self.last_touched_neuron = None
-#         self.d_w_uber_freq = {}
-#         self.global_state = {
-#             "inactive": set(),
-#             "active": set(),
-#             "predict": set()
-#         }
-
-#     def add_neuron(self, key):
-#         debug("\t\tadding neuron to column {}".format(_key))
-#         nw_nrn = CountingNeuron(_key)
-#         self.columns[_key].add(nw_nrn)
-
-#     def reset(self):
-#         self.clear_global_counter()
-#         self.clear_word_counter()
-
-#     def get_global_counter(self):
-#         return self.global_counter
-#     def get_word_counter(self):
-#         return self.word_counter
-#     def set_window(self, window):
-#         self.window_size = window
-#     def clear_word_counter(self):
-#         self.word_counter = Counter()
-#     def clear_global_counter(self):
-#         self.global_counter = Counter()
-
-#     def sequencer(self, str_rep):
-#         assert type(str_rep) == type(""), "sequencer type must be string"
-#         return [[word.strip()] for word in str_rep.split(' ')]
-
-#     def tokenize(self, sentence):
-#         return [str(word.strip().strip('\r\n\t.')) for word in sentence.strip().split(' ')]
-
-#     def train_from_file(self, filepath):
-#         print("Loading file {}".format(filepath))
-#         with open(filepath,'r') as source:
-#             for idx, sentence in enumerate(source):
-#                 if idx % 10000 == 0: print(idx)
-#                 self.last_touched_neuron = None
-#                 self.simple_hit([word for word in self.tokenize(sentence)])
-
-#     def simple_hit(self, sequence):
-#         debug(sequence)
-#         for _key in sequence:
-#             debug("\t\tadding neuron to column {}".format(_key))
-#             nw_nrn = self.add_neuron(_key)
-
-#             if self.last_touched_neuron != None:
-#                 self.last_touched_neuron.add_upstream(nw_nrn)
-#                 nw_nrn.add_downstream(self.last_touched_neuron)
-#             self.last_touched_neuron = nw_nrn
-
-#     def initialize_frequency_dict(self):
-#         total_neurons = len(self.get_number_neurons_per_key())
-#         d_w_uber_freq = {}
-#         for word, count in self.get_number_neurons_per_key().most_common():
-#             d_w_uber_freq[word] = float(count)/total_neurons
-#         self.total_neurons = total_neurons
-#         self.d_w_uber_freq = d_w_uber_freq
-#         return total_neurons, d_w_uber_freq
-
-#     def comparison_frequencies(self, the_WORD, ratio=0.05, cutoff=15, visualize_it=False):
-#         word_test, total_spec_w = self.get_counts_for_specific_key(the_WORD).most_common(1)[0] # should be itself
-#         print("Count for ", word_test," is ", total_spec_w)
-#         arr_the_word = []
-#         arr_global_f = []
-#         arr_spec_f   = []
-
-#         for word, count in self.get_counts_for_specific_key(the_WORD).most_common():
-#             this_freq = float(count/(total_spec_w + 0.01))
-#             if float(self.d_w_uber_freq[word]/this_freq) <= ratio:
-#                 arr_the_word.append(word)
-#                 arr_global_f.append(self.d_w_uber_freq[word])
-#                 arr_spec_f.append(this_freq)
-#                 if len(arr_the_word) > cutoff:
-#                     break
-
-#         print("Going to start visual with {}".format(arr_the_word))
-#         if visualize_it:
-#             fig, ax = plt.subplots()
-#             ax.scatter(arr_spec_f[1:cutoff], arr_global_f[1:cutoff])
-
-#             for i, txt in enumerate(arr_the_word[1:cutoff]):
-#                 ax.annotate(txt, (arr_spec_f[i+1],arr_global_f[i+1]))
-#             arr_the_word[1:cutoff]
-#             plt.show()
-
-#         return arr_global_f[:cutoff], arr_spec_f[:cutoff], arr_the_word[:cutoff]
-
-#     def get_counts_for_specific_key(self, key):
-#         self.word_counter.clear()
-#         for neuron in self.columns[key]:
-#             neuron.propagate_up(self.word_counter, self.window_size)
-#             neuron.propagate_dn(self.word_counter, self.window_size)
-#         self.word_counter[key] = self.word_counter[key]/2
-#         return self.word_counter
-
-#     def get_number_neurons_per_key(self):
-#         d_nums={}
-#         for key, neurons in self.columns.items():
-#             d_nums[key] = len(neurons)
-#         return Counter(d_nums)
-
-
-
-#     def get_predicted_counts_from_lighting_columns(self, lst_keys):
-#         self.full_reset()
-#         for key in lst_keys:
-#             self.light_column(key)
-#         predicted_neurons = self.global_state["predict"]
-#         lst = []
-#         for neuron in predicted_neurons:
-#             [lst.append(key) for key in neuron.keys]
-
-#         return Counter(lst)
-
-#     def compare_two_words(self, w1, w2, nhits=100):
-#         common_w1 = self.get_counts_for_specific_key(w1).most_common(nhits)
-#         common_w2 = self.get_counts_for_specific_key(w2).most_common(nhits)
-#         set_w1 = set([k for k, v in common_w1])
-#         set_w2 = set([k for k, v in common_w2])
-#         return set_w1 - set_w2
-
-#     def related_to_word(self, w1, remove_common=True, nhits=100, nstops=100):
-#         common_w1 = self.get_counts_for_specific_key(w1).most_common(nhits)
-#         set_w1 = set([k for k, v in common_w1])
-#         if remove_common:
-#             stop_words = self.get_number_neurons_per_key().most_common(nstops)
-#             set_stop_words = set([k for k, v in stop_words])
-#             return set_w1 - set_stop_words
-#         else:
-#             return set_w1
-
-
-
-#     def get_top_x_words_in_layer(self, x):
-#         """Return list of the top x words with the highest count in the layer"""
-#         return [word for word, _ in layer.get_number_neurons_per_key().most_common()[:x]]
